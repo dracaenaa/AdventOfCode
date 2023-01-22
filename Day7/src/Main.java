5c6f9f5cc9776b03f0cfdb09e9aeb8450c6c21cf
Miranda Kolker <mkolker@bgsu.edu>
2023-01-22 15:47:26 -0500

    dir nav is working

--- Day7/src/Main.java
+++ Day7/src/Main.java
@@ -12,34 +12,47 @@ public class Main {
 
         ArrayList<String> input = handleFileReading();
         Map<String, Directory> directories = new HashMap<>();
+        String curDirName = "";
 
         for (int i = 0; i < input.size(); i++) {
             String curLine = input.get(i);
             if (curLine.contains("dir")) {
                 if (!directories.containsKey(curLine.substring(4))) {
-                    Directory newDir = handleCreateDirectory(curLine.substring(4));
+                    Directory newDir = handleCreateDirectory("/" + curLine.substring(4));
                     directories.put(newDir.getName(), newDir);
                 }
-            } else if (curLine.contains("cd") && !curLine.contains("..") && !curLine.contains("ls") && !curLine.contains("/")) {
-                String curDirName = curLine.substring(5);
-                Directory curDir = directories.get(curDirName);
-                while (i < input.size()-1) {
-                    i++;
-                    curLine = input.get(i);
-                    if (curLine.contains("dir")) {
-                        String subDirName = curDirName + "/" + curLine.substring(4);
-                        if(!directories.containsKey(subDirName)) {
-                            Directory newDir = handleCreateDirectory(subDirName);
-                            directories.put(newDir.getName(), newDir);
+            }
+            else if (curLine.contains("cd")) {
+                if (!curLine.contains("..") && !curLine.contains("/")) {
+                    curDirName = curDirName.concat("/" + curLine.substring(5));
+                    Directory curDir = directories.get(curDirName);
+                    if (curDir == null) {
+                        curDir = handleCreateDirectory(curDirName);
+                    }
+                    while (i < input.size() - 1) {
+                        i++;
+                        curLine = input.get(i);
+                        if (curLine.contains("dir")) {
+                            String subDirName = curDirName + "/" + curLine.substring(4);
+                            if (!directories.containsKey(subDirName)) {
+                                Directory newDir = handleCreateDirectory(subDirName);
+                                directories.put(newDir.getName(), newDir);
+                            }
+                            curDir.addSubdirectory(subDirName, directories.get(subDirName));
+                        } else if (!curLine.contains("dir") && !curLine.contains("$")) {
+                            curDir.setSize(Integer.parseInt(curLine.split(" ")[0]));
+                        } else if (curLine.contains("$ cd")) {
+                            i--;
+                            break;
                         }
-                        curDir.addSubdirectory(subDirName, directories.get(subDirName));
                     }
-                    else if (!curLine.contains("dir") && !curLine.contains("$")) {
-                        curDir.setSize(Integer.parseInt(curLine.split(" ")[0]));
-                    }
-                    else if(curLine.contains("$ cd ..")) {
-                        i--;
-                        break;
+                }
+                else if (curLine.contains("..") &&!curLine.contains("/")) {
+                    String[] splitName = curDirName.split("/");
+                    curDirName = "";
+                    for (int j = 1; j < splitName.length; j++) {
+                        if (j == splitName.length-1) break;
+                        curDirName = curDirName.concat("/" + splitName[j]);
                     }
                 }
             }
